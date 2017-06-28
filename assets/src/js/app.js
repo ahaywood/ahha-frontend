8a0ddd3951dc659a1039d4dc86f3c639f077f6e3
Amy Dutton <amy@ahhacreative.com>
2017-06-28 17:15:17 -0500

    Updated JavaScript
    * Included JavaScript files by default
    * Added jQuery
    * Created a default function for FitVids for handling YouTube and Vimeo

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,3 +1,4 @@
+//= include ../../../bower_components/jquery/dist/jquery.js
 //= include ../../../bower_components/jquery.fitvids/jquery.fitvids.js
 
 jQuery ( function($) {
@@ -14,8 +15,15 @@ function SiteController($)
 		initFitVid();
 	}
 
+	// MAKES VIDEOS RESIZE RESPONSIVELY
 	function initFitVid() {
-		
+
+		// TARGET YOUTUBE
+		$('iframe[src*="youtube"]').parent().fitVids();
+
+		// TARGET VIMEO
+		$('iframe[src*="vimeo"]').parent().fitVids();
+
 	}
 
 	return self;
